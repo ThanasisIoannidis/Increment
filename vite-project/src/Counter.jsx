ac9b0565a3ffab78383dd84c0fab6a3e0f60edeb
Thanasis <thanasisfirst@gmail.com>
2024-08-18 01:01:44 +0100

    Version2

--- vite-project/src/Counter.jsx
+++ vite-project/src/Counter.jsx
@@ -8,10 +8,20 @@ function Counter(){
         setCount(count + 1);
     }
 
+    const reset = () => {
+        setCount(0);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
     return(<div>
         <h1>Welcome to the Counter!</h1>
         <p className="count-display">{count}</p>
-        <button className="Increment-button" onClick={increment}>Increment</button>
+        <button className="increment-button" onClick={increment}>Increment</button>
+        <button className="reset-button" onClick={reset}>Reset</button>
+        <button className="decrement-button" onClick={decrement}>Decrement</button>
     </div>); 
 }
 export default Counter 
